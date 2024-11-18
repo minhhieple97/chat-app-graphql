3eafa249e433e6d9883697c78d2ed14a3e5ce50f
hieple <hieplevuc@gmail.com>
2024-11-18 14:57:09 +0700

    feat: Setup chatroom crud

--- server/src/chatroom/chatroom.module.ts
+++ server/src/chatroom/chatroom.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ChatroomService } from './chatroom.service';
+import { ChatroomResolver } from './chatroom.resolver';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { PrismaService } from 'src/prisma/prisma.service';
 
-@Module({})
+@Module({
+  imports: [PrismaModule],
+  providers: [ChatroomService, ChatroomResolver, PrismaService],
+})
 export class ChatroomModule {}
